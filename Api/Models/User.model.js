60bf3a33eccc37a41460a2b9f9f045b9eceb3663
duc1504 <ngocducofficial@gmail.com>
2024-04-07 01:19:41 +0700

    edit user to users

--- Api/Models/User.model.js
+++ Api/Models/User.model.js
@@ -40,6 +40,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema, "user");
+const User = mongoose.model("User", userSchema, "users");
 
 module.exports = User;
