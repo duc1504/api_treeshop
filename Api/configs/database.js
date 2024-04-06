14d7219dc202bdf81b145fcb09175d71adc8f700
duc1504 <ngocducofficial@gmail.com>
2024-04-07 01:07:47 +0700

    Resolve conflicts

--- Api/configs/database.js
+++ Api/configs/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports.connect= async()=>{
     
     try {
-        await mongoose.connect(process.env.MONGO_ATLAt);
+        await mongoose.connect(process.env.MONGO_ATLAT);
         console.log("Connected to successfully")
      } catch (error) {
          console.log("Connected to failed")
